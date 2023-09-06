e3fd2ef78d5522329cbd9be7816805f20712be6c
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-09-06 17:30:06 -0500

    add Debonair

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,3 +1,14 @@
 const pageName = "Empirical";
 
-export { pageName };
+// page thumbnail
+import debonairThumbnail from "../assets/pageImg/debonair.jpg";
+
+const pages = [
+  {
+    id: 1,
+    name: "debonair",
+    thumbnail: debonairThumbnail,
+  },
+];
+
+export { pageName, pages };
