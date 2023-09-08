25121029f58526b2a1d54e13e47f8919855ddb5b
gustavo-rbl <141947854+gustavo-rbl@users.noreply.github.com>
2023-09-07 20:27:45 -0500

    add Balmy

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,18 @@ const pageName = "Empirical";
 
 // page thumbnail
 import debonairThumbnail from "../assets/pageImg/debonair.jpg";
+import balmyThumbnail from "../assets/pageImg/balmy.jpg";
 
 const pages = [
   {
     id: 1,
     name: "debonair",
     thumbnail: debonairThumbnail,
+  },
+  {
+    id: 2,
+    name: "balmy",
+    thumbnail: balmyThumbnail,
   },
 ];
 
